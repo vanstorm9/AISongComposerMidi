15f2cea68a73bc27cbc5cc49de983aa848bd79aa
Anthony Lowhur <antlowhur@yahoo.com>
2015-10-27 21:17:02 -0400

    Managed to improve the visualization of clustered data as well as adding violin songs

--- clustering.py
+++ clustering.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+from time import time
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
 import midi
 from midiutil.MidiFile import MIDIFile
 import os
@@ -9,25 +17,21 @@ import os.path
 
 
 
-#path = 'example.mid'
-#path = 'Songs/Suteki-Da-Ne.mid'
-#path = 'Songs/Mozart-Movement.mid'
-#path = 'Songs/beethoven_ode_to_joy.mid'
-#path = 'Songs/twinkle_twinkle.mid'
-#path = 'Songs/grenade.mid
-
 slash = '/'
 
 print 'Extracting all of pattern[1]'
 # Instantiate a MIDI Pattern (contains a list of tracks)
 pat = midi.Pattern()
 
-#folder_trans = 'training-songs'
-#folder_trans = 'instruments'
-folder_trans = np.array(['instruments/guitar','instruments/piano'])
-#folder_trans = 'training-video-test'
-#folder_trans = 'training-kid-songs'
-#folder_trans = 'training-classical-songs'
+
+# Array of directories to transfers through
+folder_trans = np.array(['instruments/guitar',
+                         'instruments/piano',
+                         'instruments/violin'])
+
+# Set number of clusters
+num_of_clusters = 3
+
 '''
 num_files = len([f for f in os.listdir(folder_trans)
                     if os.path.isfile(os.path.join(folder_trans, f))])
@@ -52,11 +56,12 @@ def pitch_prev_array_add(pitch, pitch_ar):
 def tranverse_all_folders(folder_trans):
     j = 0
     k = 0
-
+    skip = False
     label_ar = np.array([])
     while k < folder_trans.size:
         
         for path in os.listdir(folder_trans[k]):
+            #print path
             pattern = midi.read_midifile(folder_trans[k] + slash + path)
             print folder_trans[k] + slash + path
             # Instantiate a MIDI Track (contains a list of MIDI events)
@@ -69,23 +74,32 @@ def tranverse_all_folders(folder_trans):
             temp = np.array([k])
             label_ar = np.concatenate((label_ar, temp))
 
-            '''
-            tr = 1
-            start_val = 1
-            i = 1
-            '''
-            # Grenade sample window
-            '''
-            tr = 5
-            start_val = 80
-            i = 80
-            '''
-            # Suteki Da Ne sample window
-            tr = 2
+
+            # Midi file track information
+            tr = 0
             start_val = 1
             i = 1
             limit = 200
-            #print len(pattern[tr])
+
+
+
+            # To choose track that has enough notes
+            p = 0
+            exc = True
+
+            
+            while p < len(pattern):
+                if len(pattern[tr]) >= limit:
+                    exc = False
+                    break
+                tr = tr + 1
+                p = p + 1
+
+            if exc:
+                print "All of this song's tracks does not have enough notes"
+                quit()
+
+                
             while True:
 
                 # This is the if statement to break out of loop
@@ -94,14 +108,13 @@ def tranverse_all_folders(folder_trans):
                 if i > limit:
                     break
                 #print pattern[tr][i]
+         
+                
                 tick = pattern[tr][i].tick
 
                 #print pattern[tr][i].data
                 #print len(pattern[tr][i].data)
                 
-                if len(pattern[tr][i].data) == 0:
-                    print 'Entering'
-                    break
                 
                 pitch = pattern[tr][i].data[0]
 
@@ -128,6 +141,11 @@ def tranverse_all_folders(folder_trans):
                 i = i + 1
             
             #print tick_ar.shape
+            if skip:
+                j= j + 1
+                continue
+
+                
             if j == 0:
                 tick_u_ar = tick_ar
                 velocity_u_ar = velocity_ar
@@ -146,6 +164,97 @@ def tranverse_all_folders(folder_trans):
             
     return pattern, tick_u_ar, velocity_u_ar, pitch_u_ar, label_ar
 
+
+
+
+def threeD_plot(X,y, label_ar,num_of_clusters):
+    np.random.seed(42)
+
+    #digits = load_digits()
+    data = scale(X)
+
+    n_samples, n_features = data.shape
+    n_digits = num_of_clusters
+    labels = y
+
+    sample_size = 300
+
+    print("n_digits: %d, \t n_samples %d, \t n_features %d"
+          % (n_digits, n_samples, n_features))
+
+
+    print(79 * '_')
+    print('% 9s' % 'init'
+          '    time  inertia    homo   compl  v-meas     ARI AMI  silhouette')
+
+    def bench_k_means(estimator, name, data):
+        t0 = time()
+        estimator.fit(data)
+        print('% 9s   %.2fs    %i   %.3f   %.3f   %.3f   %.3f   %.3f    %.3f'
+              % (name, (time() - t0), estimator.inertia_,
+                 metrics.homogeneity_score(labels, estimator.labels_),
+                 metrics.completeness_score(labels, estimator.labels_),
+                 metrics.v_measure_score(labels, estimator.labels_),
+                 metrics.adjusted_rand_score(labels, estimator.labels_),
+                 metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
+                 metrics.silhouette_score(data, estimator.labels_,
+                                          metric='euclidean',
+                                          sample_size=sample_size)))
+
+
+    bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
+                  name="k-means++", data=data)
+
+    bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
+                  name="random", data=data)
+
+    # in this case the seeding of the centers is deterministic, hence we run the
+    # kmeans algorithm only once with n_init=1
+    pca = PCA(n_components=n_digits).fit(data)
+    bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),
+                  name="PCA-based",
+                  data=data)
+    print(79 * '_')
+
+    ###############################################################################
+    # Visualize the results on PCA-reduced data
+
+    reduced_data = PCA(n_components=2).fit_transform(data)
+    kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
+    kmeans.fit(reduced_data)
+
+    # Step size of the mesh. Decrease to increase the quality of the VQ.
+    h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    x_min, x_max = reduced_data[:, 0].min() + 1, reduced_data[:, 0].max() - 1
+    y_min, y_max = reduced_data[:, 1].min() + 1, reduced_data[:, 1].max() - 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Obtain labels for each point in mesh. Use last trained model.
+    Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure(1)
+    plt.clf()
+    plt.imshow(Z, interpolation='nearest',
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.cm.Paired,
+               aspect='auto', origin='lower')
+
+    plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=8)
+    # Plot the centroids as a white X
+    centroids = kmeans.cluster_centers_
+    plt.scatter(centroids[:, 0], centroids[:, 1],
+                marker='x', s=169, linewidths=3,
+                color='w', zorder=10)
+    plt.title('Clustering pitches between Guitar, Piano, and Violin')
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
 
 
 
@@ -171,69 +280,14 @@ print uni_ar.shape
 #### Clustering Code ###
 
 
-from mpl_toolkits.mplot3d import Axes3D
-
-
-from sklearn.cluster import KMeans
-np.random.seed(42)
-
-centers = [[1, 1], [-1, -1], [1, -1]]
-X = tick_ar
-#X = pitch_ar
+#X = tick_ar
+X = pitch_ar
 y = label_ar
 
-estimators = {'k_means_iris_3': KMeans(n_clusters=2),
-              'k_means_iris_8': KMeans(n_clusters=2),
-              'k_means_iris_bad_init': KMeans(n_clusters=2, n_init=1,
-                                              init='random')}
+
+threeD_plot(X, y, label_ar,num_of_clusters)
 
 
-fignum = 1
-for name, est in estimators.items():
-    fig = plt.figure(fignum, figsize=(4, 3))
-    plt.clf()
-    ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
 
-    plt.cla()
-    est.fit(X)
-    labels = est.labels_
-
-    ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=labels.astype(np.float))
-
-    ax.w_xaxis.set_ticklabels([])
-    ax.w_yaxis.set_ticklabels([])
-    ax.w_zaxis.set_ticklabels([])
-    ax.set_xlabel('Petal width')
-    ax.set_ylabel('Sepal length')
-    ax.set_zlabel('Ticks')
-    fignum = fignum + 1
-
-# Plot the ground truth
-fig = plt.figure(fignum, figsize=(4, 3))
-plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
-plt.cla()
-'''
-for name, label in [('Setosa', 0),
-                    ('Versicolour', 1),
-                    ('Virginica', 2)]:
-    ax.text3D(X[y == label, 3].mean(),
-              X[y == label, 0].mean() + 1.5,
-              X[y == label, 2].mean(), name,
-              horizontalalignment='center',
-              bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-'''
-# Reorder the labels to have colors matching the cluster results
-#y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 3], X[:, 0], X[:, 2], c=y)
-
-ax.w_xaxis.set_ticklabels([])
-ax.w_yaxis.set_ticklabels([])
-ax.w_zaxis.set_ticklabels([])
-ax.set_xlabel('Petal width')
-ax.set_ylabel('Sepal length')
-ax.set_zlabel('Ticks')
-plt.show()
 
 
